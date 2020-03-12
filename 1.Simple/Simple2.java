6ebb9264bea3fb71395947538e37075165a3991a
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2020-03-12 09:48:39 +0530

    Update Simple2.java

--- 1.Simple/Simple2.java
+++ 1.Simple/Simple2.java
@@ -4,10 +4,10 @@ class Simple2
 {
 	public static void main(String [] args)
 	{
-		int num;
-		num = 100;
+		int num; //Declearing Integer Variable
+		num = 100; // Assigning Value to variable
 		System.out.println("This is Num :-"+num);
-		num = num * 2;
+		num = num * 2; // Multiplication 
 		System.out.print("This is Num * 2:-");
 		System.out.println(num);
 	}
