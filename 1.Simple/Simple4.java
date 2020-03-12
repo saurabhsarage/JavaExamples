6c4f82d857b8306f467903534ee4ca664dd18ddb
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2020-03-12 10:06:20 +0530

    Update Simple4.java

--- 1.Simple/Simple4.java
+++ 1.Simple/Simple4.java
@@ -1,4 +1,5 @@
 //This is For LOOP
+// Demonstration for the "For loop"
 class Simple4
 {
 	public static void main(String [] args)
@@ -6,6 +7,7 @@ class Simple4
 		int x;
 		for(x=1;x<=10;x=x+1)
 		{
+			// This Loop will Print Number from 1 to 10
 			System.out.println("This is X :- "+x);
 		}
 	}
