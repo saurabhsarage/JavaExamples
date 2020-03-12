0441ea68e235bd4a5c3bbf5283deed3f07872a9f
Saurabh Sarage <57210145+saurabhsarage@users.noreply.github.com>
2020-03-12 10:08:05 +0530

    Update Simple5.java

--- 1.Simple/Simple5.java
+++ 1.Simple/Simple5.java
@@ -3,13 +3,13 @@ class Simple5
 {
 	public static void main(String args [])
 	{
-		int x,$y;
-		$y = 20;
+		int x,y;
+		y = 20;
 		for(x=0;x<10;x++)
 		{
 			System.out.println("This Is X :- "+x);
-			System.out.println("This Is Y :- "+$y);
-			$y = $y - 2;
+			System.out.println("This Is Y :- "+y);
+			y = y - 2;
 		}
 	}
 }
